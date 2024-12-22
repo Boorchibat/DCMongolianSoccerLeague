947272fc01d484a54ec028cf475d9a56948aed43
Boorchi Batzorigt <boorchibatzorigt@Michidmaas-MacBook-Air.local>
2024-12-22 17:36:48 -0500

    added pages

---
+++ pages/clubs/Club.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Club.css"
+
+export const Club = () => {
+  return (
+    <div>Club</div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div >
-      <header >
-        hello world
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>homepage</div>}/>
+        <Route path="/results" element={<div>results</div>}/>
+        <Route path="/table" element={<div>table</div>}/>
+        <Route path="/clubs" element={<div>clubs</div>}/>
+        <Route path="/red-hawks" element={<div>red-hawks</div>}/>
+        <Route path="/jaguars" element={<div>jaguars</div>}/>
+        <Route path="/mcc" element={<div>mcc</div>}/>
+        <Route path="/dc-hawks" element={<div>dc-hawks</div>}/>
+        <Route path="/wolves" element={<div>wolves</div>}/>
+        <Route path="/dc-masters" element={<div>dc masters</div>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
