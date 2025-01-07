2e51cafe9eff0983dd8002ede7ead6fdb76a7527
Boorchi Batzorigt <coolboyboorchi@gmail.com>
2025-01-07 15:20:05 -0500

    Added header and footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,39 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div id="main-footer">
+      <div id="smaller-footer">
+        <img id="image" src="mfb-img1.png" alt="s" />
+        <div id="divz">
+          <div id="headline">
+            <div className="column">
+                <h1>League</h1>
+                <p>Home</p>
+                <p>Table</p>
+                <p>Results</p>
+                <>Clubs</>
+            </div>
+            <div className="column">
+              <h1>Stats</h1>
+              <p>Stats page</p>
+              <p>Teams</p>
+              <p>Table</p>
+            </div>
+            <div className="column">
+              <h1>Contact</h1>
+              <p>FaceBook manager Contact</p>
+              <p>League administrator contact</p>
+              <p>Page Creater Contact</p>
+            </div>
+            <div className="column">
+                <h1>Information </h1>
+                <p>This Website was made to give information about the league</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Home.css"
 import { Header } from '../../components/header/Header'
+import { Footer } from '../../components/footer/Footer'
 
 export const HomePage = () => {
   return (
     <div>
        <Header/>
+       <Footer/>
     </div>
   )
 }
